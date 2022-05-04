2c51d5db4aa2fd15b4ad845da99d725d5ed9e7ca
brett <71146138+Phantom77-x@users.noreply.github.com>
2022-05-04 18:25:22 -0400

    Added and removed some special characters

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randint
 
 if __name__ == "__main__":
 
-    specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "?", "<", ">", "/", "|", "`", "~", ";", ":", ",", ".", "-", "+", "="]
+    specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "?", "<", ">", "/", "_", "~", "'", ":", "-", "+", "="]
     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -54,7 +54,7 @@ if __name__ == "__main__":
         list[position], list[i] = list[i], list[position]  
     finalPassword = ""
     for i in range(strLength):
-        res = res + list[i] 
+        finalPassword = finalPassword + list[i] 
 
     print("Strong password: " + finalPassword)
 
