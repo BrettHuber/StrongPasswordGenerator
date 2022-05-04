dea8e05496e116d457ca114a072f6706562bac26
brett <71146138+Phantom77-x@users.noreply.github.com>
2022-05-04 17:02:49 -0400

    Added for loop to create password

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,3 +1,5 @@
+import random
+
 specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "?", "<", ">", "/", "|", "`", "~", ";", ":", ",", ".", "-", "+", "="]
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -7,4 +9,30 @@ minLength = 12
 maxLength = 24
  
 strongPassword = ""
+passwordLength = random.randrange(12,25)
+
+for x in range(passwordLength):
+    if(x == 1):
+        charNum = random.randrange(0, len(capitalLetters) + 1)
+        strongPassword += capitalLetters[charNum]
+    elif(x % 5 == 0):
+        charNum = random.randrange(0, len(capitalLetters) + 1)
+        strongPassword += capitalLetters[charNum] 
+    elif(x % 4 == 0):
+        charNum = random.randrange(0, len(numbers) + 1)
+        strongPassword += numbers[charNum]
+    elif(x % 3 == 0):
+        charNum = random.randrange(0, len(letters) + 1)
+        strongPassword += letters[charNum] 
+    elif(x % 2 == 0 or x % 7 == 0):
+        charNum = random.randrange(0, len(specialCharacters) + 1)
+        strongPassword += specialCharacters[charNum]
+    else:
+        charNum = random.randrange(0, len(letters) + 1)
+        strongPassword += letters[charNum] 
+
+
+
+
+
 
